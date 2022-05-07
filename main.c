17cbaf5071aa1a04caf116d87f45742c76912b49
mahmut <leipzig9998@hotmail.com>
2022-05-07 20:46:16 +0300

    insertion sort code updated, 2 test case arrays added

--- main.c
+++ main.c
@@ -1,41 +1,66 @@
 #include<stdio.h>
 
+//Global value
+int numberOfBasicOp=0;
+int insertionSort(int array[], int size,int kthElement) {
+    numberOfBasicOp=0;
+    for (int step = 1; step < size; step++) {
+        int key = array[step];
+        int j = step - 1;
 
-int insert(int a[], int n, int kthElement)/*function to sort an array with insertion sort*/
-{
-    int i, j, temp;
-    for (i = 1; i < n; i++) {
-        temp = a[i];
-        j = i - 1;
-
-        while (j >= 0 && temp <= a[j])/*Move the elements greater than temp to one position ahead from their current position*/
-        {
-            a[j + 1] = a[j];
-            j = j - 1;
+        // Compare key with each element on the left of it until an element smaller than
+        // it is found.
+        // For descending order, change key<array[j] to key>array[j].
+        while (++numberOfBasicOp &&key < array[j] && j >= 0 ) {
+            array[j + 1] = array[j];
+            --j;
         }
-        a[j + 1] = temp;
+        array[j + 1] = key;
     }
-    return a[kthElement - 1];
+    return array[kthElement-1];
 }
 
 void printArr(int a[], int n)/*function to print the array*/
 {
     int i;
     for (i = 0; i < n; i++)
-        printf("%d", a[i]);
+        printf("%d ", a[i]);
+    printf("\n");
 }
 
 int main() {
-    int a[] = {12, 31, 25, 8, 32, 17};
+    int a[] = {10, 9, 8, 7, 6, 5};
+    int b[]={5,6,7,8,9,10};
     int n = sizeof(a) / sizeof(a[0]);
+    int nb = sizeof(b) / sizeof(b[0]);
+
     int kthElement = 2;
     setbuf(stdout, 0);
     printf("Before sorting array elements are-\n");
     printArr(a, n);
-    insert(a, n, kthElement);
+
+    //Insertion sort for the worst case
+   insertionSort(a, n, kthElement);
     printf("\nAfter sorting array elements are-\n");
     printArr(a, n);
-    printf("Printing kth element %d", insert(a, n, kthElement));
-    return 0;
+    printf("Number of operations %d\n",numberOfBasicOp);
+    kthElement=2;
+    printf("Printing kth element %d\n", insertionSort(a, n, kthElement));
 
+    printf("------------------------------------------------------------------");
+    //Insertion sort for the best case
+
+    setbuf(stdout, 0);
+    printf("\nBefore sorting array elements are-\n");
+    printArr(b, n);
+    insertionSort(b, n, kthElement);
+    printf("\nAfter sorting array elements are-\n");
+    printArr(b, n);
+    printf("Number of operations %d\n",numberOfBasicOp);
+    kthElement=2;
+    printf("Printing kth element %d\n", insertionSort(b, nb, kthElement));
+
+
+
+    return 0;
 }

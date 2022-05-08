809f2644d2008ce56ea1a5fb164ed3b8078a21a9
mahmut <leipzig9998@hotmail.com>
2022-05-08 12:14:38 +0300

    quick sort added. Best case input txt for quick sort needs to be added

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
-
+#include <string.h>
 //Global value
 int numberOfBasicOp=0;
-int insertionSort(int array[], int size,int kthElement) {
+void insertionSort(int array[], int size) {
     numberOfBasicOp=0;
     for (int step = 1; step < size; step++) {
         int key = array[step];
@@ -17,7 +17,61 @@ int insertionSort(int array[], int size,int kthElement) {
         }
         array[j + 1] = key;
     }
-    return array[kthElement-1];
+
+}
+
+// Quick-sort algorithm
+// function to swap elements
+void swap(int *a, int *b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+// function to find the partition position
+int partition(int array[], int low, int high) {
+
+    // select the rightmost element as pivot
+    int pivot = array[high];
+
+    // pointer for greater element
+    int i = (low - 1);
+
+    // traverse each element of the array
+    // compare them with the pivot
+    for (int j = low; j < high; j++) {
+        if (++numberOfBasicOp && array[j] <= pivot) {
+
+            // if element smaller than pivot is found
+            // swap it with the greater element pointed by i
+            i++;
+
+            // swap element at i with element at j
+            swap(&array[i], &array[j]);
+        }
+    }
+
+    // swap the pivot element with the greater element at i
+    swap(&array[i + 1], &array[high]);
+
+    // return the partition point
+    return (i + 1);
+}
+
+void quickSort(int array[], int low, int high) {
+    if (low < high) {
+
+        // find the pivot element such that
+        // elements smaller than pivot are on left of pivot
+        // elements greater than pivot are on right of pivot
+        int pi = partition(array, low, high);
+
+        // recursive call on the left of pivot
+        quickSort(array, low, pi - 1);
+
+        // recursive call on the right of pivot
+        quickSort(array, pi + 1, high);
+    }
 }
 
 void printArr(int a[], int n)/*function to print the array*/
@@ -27,40 +81,74 @@ void printArr(int a[], int n)/*function to print the array*/
         printf("%d ", a[i]);
     printf("\n");
 }
+int returnKthElement(int a[],int k){
+    return a[k-1];
+}
+void read(int array[],char *inputFile){
+    int index=0;
+    int entity=0;
+    FILE *fPtr;
+    char fileNamex[100];
+    strcpy(fileNamex, "C:\\Users\\asxdc\\CLionProjects\\AnalysisOfAlgorithmProject\\");
+    //inputFile ex : input.txt
+    strcat(fileNamex, inputFile);
+    fPtr = fopen(fileNamex, "r");
+    if (fPtr == NULL) {
+        printf("There is a error opening the file.");
+
+    }
+    while (fscanf(fPtr, "%d", &entity) != EOF) {
+        array[index]=entity;
+        index++;
+    }
+
+
+}
+
+// SELECTION SORT
+
 
 int main() {
-    int a[] = {10, 9, 8, 7, 6, 5};
-    int b[]={5,6,7,8,9,10};
-    int n = sizeof(a) / sizeof(a[0]);
-    int nb = sizeof(b) / sizeof(b[0]);
+    int iterator=0;
+    while(iterator<10){
+        printf("Enter algorithm type:          --Insertion: 1,Quicksort: 3 \n");
+        int option;
+        scanf("%d", &option);
 
-    int kthElement = 2;
-    setbuf(stdout, 0);
-    printf("Before sorting array elements are-\n");
-    printArr(a, n);
+        //For Insertion sort
+        if(option==1){
+            int a[15];
+            int kthElement=0;
+            setbuf(stdout, 0);
+            printf("%s", "Please enter the name of the file :");
+            char filename[20];
+            scanf("%s", filename);
+            read(a,filename);
+            numberOfBasicOp=0;
+            insertionSort(a,15);
+            printf("Number of comparasions %d\n",numberOfBasicOp);
+            printf("k th element?");
+            scanf("%d",&kthElement);
+            printf("%d\n", returnKthElement(a,kthElement));
+        }
+        // For Quicksort
+        else if(option==3){
+            int a[15];
+            int kthElement=0;
+            setbuf(stdout, 0);
+            printf("%s", "Please enter the name of the file :");
+            char filename[20];
+            scanf("%s", filename);
+            read(a,filename);
+            numberOfBasicOp=0;
+            quickSort(a,0,14);
+            printf("Number of comparasions %d\n",numberOfBasicOp);
+            printf("k th element?");
+            scanf("%d",&kthElement);
+            printf("%d\n", returnKthElement(a,kthElement));
+        }
 
-    //Insertion sort for the worst case
-   insertionSort(a, n, kthElement);
-    printf("\nAfter sorting array elements are-\n");
-    printArr(a, n);
-    printf("Number of operations %d\n",numberOfBasicOp);
-    kthElement=2;
-    printf("Printing kth element %d\n", insertionSort(a, n, kthElement));
-
-    printf("------------------------------------------------------------------");
-    //Insertion sort for the best case
-
-    setbuf(stdout, 0);
-    printf("\nBefore sorting array elements are-\n");
-    printArr(b, n);
-    insertionSort(b, n, kthElement);
-    printf("\nAfter sorting array elements are-\n");
-    printArr(b, n);
-    printf("Number of operations %d\n",numberOfBasicOp);
-    kthElement=2;
-    printf("Printing kth element %d\n", insertionSort(b, nb, kthElement));
-
-
+    }
 
     return 0;
 }
